307977f369d2432dabe108c0f483aec58e903f73
BL <bcl168@gmail.com>
2017-03-02 17:03:11 -0500

    initial commit

---
+++ MultiNews/MultiNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiNews
+//
+//  Created by bl on 3/2/17.
+//  Copyright © 2017 bl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
