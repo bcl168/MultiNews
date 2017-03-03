10b0e77007eac992fc738564e54c63b8ac14e0e3
Avery <imthatninjatho@gmail.com>
2017-03-02 21:10:26 -0500

    comment

--- MultiNews/MultiNews/ViewController.m
+++ MultiNews/MultiNews/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.newsData = @[@"cnn", @"fox"];
+    self.newsPickerView.dataSource = self;
+    self.newsPickerView.delegate = self;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +27,48 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(void)viewWillAppear:(BOOL)animated {
+    NSInteger lastSelected = [[NSUserDefaults standardUserDefaults]integerForKey:@"LastSelected"];
+    [self.newsPickerView selectRow:lastSelected inComponent:0 animated:NO];
+    
+    
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return self.newsData.count;
+}
+
+- (NSString*)pickerView:(UIPickerView *)zones titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    
+    return self.newsData[row];
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    [[NSUserDefaults standardUserDefaults]setInteger:row forKey:@"LastSelected"];
+    
+    //set last selected pickerview index in userdefaults
+    
+    [[NSUserDefaults standardUserDefaults]setInteger:row forKey:@"LastSelected"];
+    
+    //set timezone in userdefaults
+    
+    if([self.newsData[row]  isEqual: @"cnn"]){
+        [[NSUserDefaults standardUserDefaults] setObject:@"EDT" forKey:@"timeZone"];
+        
+    }
+    if([self.newsData[row]  isEqual: @"fox"]){
+        [[NSUserDefaults standardUserDefaults] setObject:@"CDT" forKey:@"timeZone"];
+        
+    }
+    
+    
+    
 }
 
 
